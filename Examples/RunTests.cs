685ab47a189bb4595a110c4fcfbca758da8f50ee
johtela <tommi.johtela@dnainternet.net>
2018-02-13 22:37:24 +0200

    Minor editing of wordings.

--- Examples/RunTests.cs
+++ Examples/RunTests.cs
@@ -18,13 +18,12 @@ namespace Examples
 		static void Main (string[] args)
 		{
 			/*
-			There are two variants for the test runner, the `RunTests` will 
-			execute test methods without gathering any timing data. 
-			`RunTestsTimed` will record how long each test takes and show the 
-			elapsed time with the results. The output will appear in the 
-			console window. If you want it somewhere else, 	use the standard 
-			`>` directive in the command line to redirect the output to a file,
-			for example.
+			There are two variants of the test runner. `RunTests` will execute 
+			test methods without gathering any timing data. `RunTestsTimed` 
+			will record how long each test takes and show the elapsed time with 
+			the results. The output will appear in the console window. If you 
+			want it someplace else, use the standard `>` directive in the 
+			command line to redirect the output to a file, for example.
 
 			Both methods take a variable list of test fixtures as an argument. 
 			Just create an instance of each fixture and pass it to the methods.
@@ -38,7 +37,7 @@ namespace Examples
 }
 /*
 ## Conclusion
-Hopefully you got some idea about how property based testing is done with 
-LinqCheck based on these few examples. Next we will jump to the implementation
-and cover all the concepts needed in LinqCheck's design.
+Hopefully you now got the basics on how property based testing is practiced 
+with LinqCheck. Next we will jump to the implementation and cover all the 
+concepts in LinqCheck's design.
 */
