9403ec5722f4d31455b37d6553ef2b806da18b2e
johtela <tommi.johtela@dnainternet.net>
2018-02-20 00:42:10 +0200

    Editing documentation for Arbitrary.cs.

--- src/Arbitrary.cs
+++ src/Arbitrary.cs
@@ -3,14 +3,30 @@
 
 ![Yin and yang](https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Yin_yang.svg/145px-Yin_yang.svg.png)
 
-In the world of property based tests generating random test data is just one
-side of the story. The true power of the technique is attained by implementing
-shrinking of values as well. If random generation is the _yin_, then the _yang_ 
-is shrinking.
+In implementing a property based testing library random data generation takes 
+us only half way there. The full power of the technique is attained by 
+implementing shrinking of values as well. If random generation is the _yin_ of
+property based testing, then the _yang_ is shrinking.
 
-We put these dual operations into the generic interface `IArbitrary<T>`. There 
-must be an instance of this interface for all the types we want to use in our
-properties.
+Shrinking, in practical terms, stands for producing a set of simpler 
+versions of a generated value. For example, given a number -132, shrinking 
+would first invert it to yield a positive number, and then make that number 
+smaller. If all of the produced alternatives keep the property failing, as a 
+last option it would return number zero, which is arguably the simplest of all
+numbers. The same principle applies to compound data structures such as
+strings and collections as well; shrinking will first try to remove elemenents 
+from them, and then shrink each remaining element individually.
+
+The point of all this is to create a simpler versions of the failing input data
+without loosing the characteristic that makes the property fail. In the same 
+manner as random generation is used to find test cases that violate the 
+specified properties, shrinking tries to systematically simplify the input to 
+its most basic form.
+
+We define the random generation and shrinking operations in the interface 
+`IArbitrary<T>` which represents an arbitrary value of type `T`. An 
+implementation of this interface needs to be provided for all the types we wish 
+to generate in our properties.
 */
 namespace LinqCheck
 {
@@ -26,51 +42,69 @@ namespace LinqCheck
 		Gen<T> Generate { get; }
 		/*
 		The `Shrink` method provides shrunk versions of a value given as 
-		parameter. The list of alternatives is returned as IEnumerable. Each
-		returned alternative is progressively simpler than the original value.
-		The more complex the generated value is, the more instances shrinking 
-		typically returns.
+		parameter. The list of alternatives is returned as an IEnumerable. Each
+		returned alternative is progressively simpler than the previous one. 
+		The more complex the generated value is, the more alternatives 
+		`Shrink` typically returns.
 		*/
 		IEnumerable<T> Shrink (T value);
 	}
 	/*
+	## Base Class for IArbitrary Implementations
+	We define also a base class that implements the `IArbitrary<T>` interface. 
+	This class mainly used internally in the LinqCheck library.
 	*/
 	public abstract class ArbitraryBase<T> : IArbitrary<T>
 	{
 		public abstract Gen<T> Generate { get; }
-
+		/*
+		The default implementation of `Shrink` returns an empty set of values.
+		So, it actually performs no shrinking at all.
+		*/
 		public virtual IEnumerable<T> Shrink (T value)
 		{
-			return new T[0];
+			return Enumerable.Empty<T> ();
 		}
 	}
-
-	/// <summary>
-	/// Concrete arbitrary for non-generic types. Takes a function to
-	/// create an arbitrary value.
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
+	/*
+	## Wrapper Class for Arbitrary Values
+	The `Arbitrary<T>` class is meant to be used as a quick template for 
+	implementing the `IArbitary<T>` interface. It takes the generator and
+	shrinker as parameters, and stores them in properties.
+	*/
 	public class Arbitrary<T> : ArbitraryBase<T>
 	{
 		public readonly Gen<T> Generator;
 		public readonly Func<T, IEnumerable<T>> Shrinker;
-
+		/*
+		The simpler version of the constructor takes just the generator, and 
+		leaves the shrinker unassigned. No shrinking is thus performed for the 
+		generated values.
+		*/
 		public Arbitrary (Gen<T> generator)
 		{
 			Generator = generator;
 		}
-
+		/*
+		The preferred version of the constructor takes both generator and 
+		shrinker.
+		*/
 		public Arbitrary (Gen<T> generator, Func<T, IEnumerable<T>> shrinker)
 		{
 			Generator = generator;
 			Shrinker = shrinker;
 		}
-
+		/*
+		The implementation of `Generate` is trivial.
+		*/
 		public override Gen<T> Generate
 		{
 			get { return Generator; }
 		}
-
+		/*
+		The overridden implementation of the `Shrink` method reverts to the 
+		inherited (no shrinking) method, if no shrinker has been given.
+		*/
 		public override IEnumerable<T> Shrink (T value)
 		{
 			return Shrinker == null ?
@@ -78,13 +112,22 @@ namespace LinqCheck
 				Shrinker (value);
 		}
 	}
-
-	/// <summary>
-	/// The basic infrastructure and extension methods for managing
-	/// and composing IArbitrary[T] interfaces.
-	/// </summary>
+	/*
+	## Extension Methods for IArbitrary
+	We define some additional operations for the IArbitrary interface as 
+	extension methods. Most of these methods concern registering and 
+	instantiating `IArbitrary<T>` implementations for different types. We use
+	a simple [Container](Container.html) to manage the registered types at 
+	runtime.
+	*/
 	public static class Arbitrary
 	{
+		/*
+		The container is stored in a private, static variable. It is created
+		in the static constructor. The built-in IArbitrary instances defined in
+		the [DefaultArbitrary](DefaultArbitrary.html) class are registered, 
+		once the container is created.
+		*/
 		private static Container _container;
 
 		static Arbitrary ()
